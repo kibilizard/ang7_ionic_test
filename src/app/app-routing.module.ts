e478a338eb78b019dac6a526658c51c1e62213b9
kibilizard <makeeva.as.work@gmail.com>
2019-06-04 16:08:23 +0300

    init commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,8 +2,20 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'home', pathMatch: 'full' },
-  { path: 'home', loadChildren: './home/home.module#HomePageModule' },
+  { path: '', redirectTo: 'datalist', pathMatch: 'full' },
+  {
+    path: 'datalist',
+    children: [
+      {
+        path: '',
+        loadChildren: './datalist/datalist.module#DatalistPageModule',
+      },
+      {
+        path: ':recipeId',
+        loadChildren: './datalist/recipe-detail/recipe-detail.module#RecipeDetailPageModule'
+      }
+    ]
+   }
 ];
 
 @NgModule({
